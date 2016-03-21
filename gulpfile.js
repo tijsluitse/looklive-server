3192b6746fa5af267ac060f7c21df19ac0e88561
Tijs Luitse - Imac <tijs.luitse@gmail.com>
2016-03-21 23:17:16 +0100

    images compress

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp'),
     svgSprite = require('gulp-svg-sprite'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+	cache = require('gulp-cache');
 
 // Concatenate JS Files
 gulp.task('scripts', function() {
     return gulp.src('./public/js/*.js')
-      .pipe(concat('main-concat.js'))
-      .pipe(gulp.dest('./public/js'));
+      	.pipe(concat('main-concat.js'))
+      	.pipe(gulp.dest('./public/js'));
 });
 
 var svgConfig = {
@@ -33,10 +35,16 @@ var svgConfig = {
     }
 };
 
+gulp.task('images', function() {
+  	return gulp.src('src/images/**/*')
+    	.pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    	.pipe(gulp.dest('./public/images/compressed'));
+});
+
 gulp.task('icons', function () {
     gulp.src('./public/icons/svg/*.svg')
         .pipe(svgSprite(svgConfig))
         .pipe(gulp.dest('./public/icons/sprite/'));
 });
 
-gulp.task('default', ['scripts', 'icons']);
+gulp.task('default', ['scripts', 'images', 'icons']);

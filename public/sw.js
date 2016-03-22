6221bfbffbbb38bad62dd838a96a41d29d72a6a3
Tijs Luitse <tijsluitse@users.noreply.github.com>
2016-03-22 14:40:16 +0100

    Updated SW with images

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,25 @@
+console.log('ServiceWorker file loaded');
+
 this.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('liveLook-feed').then(cache => {
+    caches.open('look-live-v1').then(cache => {
       return cache.addAll([
-        './',
         '../styles/style.css',
         '../js/app.js',
-        '/api/feed'
+        '/api/feed',
+        '../images/header.jpg',
+        '../images/sprite.png',
+        '../images/logo.svg'
       ]);
     })
+  );
+});
+
+this.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+      	return response || fetch(event.request)
+      })
   );
 });
